4698700054f9fab8d88d71d15084ecf0bb838cb9
Kanstantsin Kanstantsin <zetslief@gmail.com>
2026-09-15 22:45:10 +0200

    Basic components support based on tokenizer

--- HtmlT/HtmlTemplate.cs
+++ HtmlT/HtmlTemplate.cs
@@ -1,13 +1,34 @@
-﻿using System.Collections.Immutable;
+﻿using System.Text;
+
+using DoctypeHtml.Parser;
 
 namespace HtmlT;
 
 public record Component(string Content);
 
-public record HtmlTemplate(Dictionary<string, Component> components)
+public record HtmlTemplate(Dictionary<string, Component> Components)
 {
     public string Render(string page)
-        => page;
+    {
+        var output = new StringBuilder(page.Length * 2);
+        Tokenizer.Run(page.AsMemory(), t => output.Append(TokenToString(t)));
+        return output.ToString();
+    }
+
+    private string TokenToString(Token token) => token switch
+    {
+        DoctypeToken doctype => $"<!DOCTYPE {doctype.Name}>",
+        StartTagToken startTag => startTag.SelfClosing ? SelfClosingTagToString(startTag) : $"<{startTag.Name}>",
+        EndTagToken endTag => $"</{endTag.Name}>",
+        CharacterToken character => $"{character.Character}",
+        CommentToken comment => $"<!--{comment.Data}-->",
+        EndOfFileToken _ => Environment.NewLine,
+        var unknown => throw new NotImplementedException($"Token is not supported: {unknown}"),
+    };
+
+    private string SelfClosingTagToString(StartTagToken token) => Components.TryGetValue(token.Name, out var component)
+        ? component.Content
+        : $"<{token.Name} />";
 
     public class Builder
     {
